2b8bf4e98ce0a4f1794a8609510de6ba5c9a48ae
RdEl00 <elaslireda35@gmail.com>
2022-09-28 21:28:23 +0100

    QR code files

---
+++ qr_script.py
@@ -0,0 +1,14 @@
+from tqdm import tqdm
+import time
+import qrcode
+
+message = input("What do you wanna share with the world?💡 ")
+
+for i in tqdm(range(20), desc = 'tqdm() Progress Bar'):
+    time.sleep(0.001)
+    qr = qrcode.QRCode(version=1, box_size=5, border=5)
+    qr.add_data(message)
+    qr.make()
+    img = qr.make_image(fill_color = 'black', back_color = 'white')
+    img.save("qrcode.png")
+print("Your qrcode is ready🎉, Check out your current directory🤖 ")
